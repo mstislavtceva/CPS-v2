564fedfc8dc3b1da292c2903f313fe3c9d9162b6
Vera Mstislavtseva <mstislavtsevavv@gmail.com>
2024-02-27 19:28:09 +0400

    initial project on scss, webpaack

---
+++ src/js/brandsBtn.js
@@ -0,0 +1,26 @@
+// Открытие и закрытие карточек с брендами через кнопку
+const brandsBtn = document.querySelector('#brands-btn')
+const brandsItems = document.querySelectorAll('.brands__item')
+
+export default brandsBtn.addEventListener('click', function () {
+  if (brandsBtn.lastElementChild.textContent === 'Скрыть') {
+    let lastBrandsItemsArray
+
+    if (window.innerWidth > 1119) {
+      lastBrandsItemsArray = Array.from(brandsItems).slice(-3)
+    } else {
+      lastBrandsItemsArray = Array.from(brandsItems).slice(-5)
+    }
+
+    for (let lastBrandItem of lastBrandsItemsArray) {
+      lastBrandItem.style.display = 'none'
+    }
+
+    brandsBtn.lastElementChild.textContent = 'Показать все'
+  } else {
+    for (let brandItem of brandsItems) {
+      brandItem.style.display = 'block'
+    }
+    brandsBtn.lastElementChild.textContent = 'Скрыть'
+  }
+})
--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,41 @@
 import '../scss/style.scss'
+import '../../node_modules/swiper/swiper.scss'
+import '../../node_modules/swiper/modules/pagination.scss'
+
+import Swiper from 'swiper'
+import { Pagination } from 'swiper/modules'
+import brandsBtn from './brandsBtn.js'
+
+// Инициализация и управление свайпером
+let init = false
+let swiper
+
+function swiperCard() {
+  if (window.innerWidth < 768) {
+    if (!init) {
+      init = true
+      swiper = new Swiper('.swiper', {
+        modules: [Pagination],
+
+        // Optional parameters
+        direction: 'horizontal',
+        loop: true,
+        slidesPerView: 'auto',
+        spaceBetween: '16px',
+
+        // If we need pagination
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true
+        }
+      })
+    }
+  } else if (init) {
+    swiper.destroy()
+    init = false
+  }
+}
+swiperCard()
+window.addEventListener('resize', swiperCard)
 
 console.log('Works!')
